3b6d9bd0755bd4b88fef996aa74e61f0ef25a8f1
zhangqing <heyzqt@qq.com>
2020-05-11 18:35:18 +0800

    [自定义Hook]

--- src/App.js
+++ src/App.js
@@ -1,44 +1,20 @@
-import React, { useState, createContext, useContext } from "react";
+import React, {
+  useState,
+  createContext,
+  useContext,
+  useMemo,
+  useCallback,
+  useRef
+} from "react";
 
-const CountContext = createContext();
-
-const Foo = () => {
-  return (
-    <CountContext.Consumer>{(count) => <h1>{count}</h1>}</CountContext.Consumer>
-  );
-};
-
-class Bar extends React.Component {
-  static contextType = CountContext;
-  render() {
-    let value = this.context;
-    return <h1>{value}</h1>;
-  }
+function useAnmialStatus(anmialName) {
+  return 'anmial ' + anmialName + '  begin to run';
 }
 
-const Count = () => {
-  const value = useContext(CountContext);
-  return <h1>{value}</h1>;
-};
-
 function App(props) {
-  const [count, setCount] = useState(10);
-
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
-  return (
-    <div>
-      count = {count}
-      <button onClick={handleClick}>click me + 1</button>
-      <CountContext.Provider value={count}>
-        <Foo></Foo>
-        <Bar></Bar>
-        <Count></Count>
-      </CountContext.Provider>
-    </div>
-  );
+  const msg = useAnmialStatus('DOG');
+  console.log(msg);
+  return <div></div>;
 }
 
 export default App;
